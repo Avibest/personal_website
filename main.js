4a4b5210374632f9970572e4467e0ae17cef6834
Avibest <97982356+Avibest@users.noreply.github.com>
2026-01-12 12:39:41 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+import { setupNav } from "./nav.js";
+import { initReveal } from "./reveal.js";
+import { initEffects } from "./effects.js";
+import { renderProjects, setupProjectFiltering, setupProjectModal } from "./projects.js";
+
+document.getElementById("year").textContent = new Date().getFullYear();
+
+setupNav();
+initEffects();
+
+renderProjects();
+setupProjectFiltering();
+setupProjectModal();
+
+initReveal();
 
